3ed9f9a3dc135cc75729f76ab4c4cb587e4296ef
sohailIlyas1996 <51970582+sohailIlyas1996@users.noreply.github.com>
2023-10-05 00:31:55 +0100

    Your commit message here

--- components/videoplayer.js
+++ components/videoplayer.js
@@ -1,12 +1,13 @@
 // components/VideoPlayer.js
+"use client"
 import React from 'react';
-
+import MyVideo from '../public/Videos/bgVideo.mp4'
 const VideoPlayer = () => {
   return (
     
  
       <video  width="100%" height="100vh" autoPlay muted loop>
-        <source src="/videos/bgVideo.mp4" type="video/mp4" />
+        <source src={MyVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     
